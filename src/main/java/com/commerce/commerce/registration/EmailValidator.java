a710ca5b02639d26583c2a99b4bad32714c3ed44
Luis Gonzalez <gonzalezdenisluis@gmail.com>
2022-07-18 13:21:52 +0200

    EmailValidator change

--- src/main/java/com/commerce/commerce/registration/EmailValidator.java
+++ src/main/java/com/commerce/commerce/registration/EmailValidator.java
@@ -1,13 +1,18 @@
 package com.commerce.commerce.registration;
 
 
+import com.commerce.commerce.exception.EmailAlreadyExistsException;
 import lombok.AllArgsConstructor;
-import org.apache.commons.logging.Log;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
+import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
-import javax.validation.constraints.NotEmpty;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
 @Service
@@ -16,21 +21,15 @@ import java.util.function.Predicate;
 
 public class EmailValidator implements Predicate<String> {
 
+
     @Override
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public boolean test(String email) {
-        if(email.contains("@") && email.contains(".")){
-            return true;
-        }else{
-            try {
-                throw new AddressException("Invalid email: " + email);
-            } catch (AddressException e) {
-                throw new RuntimeException(e);
-            }
-
-        }
+        if (!email.contains("@") && !email.contains("."))
+            throw new EmailAlreadyExistsException("Invalid email address");
 
 
-
-
+        return false;
     }
 }
