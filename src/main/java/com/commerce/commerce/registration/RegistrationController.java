8ee225b3d656284badf67dac309da2b792549292
Luis Gonzalez <gonzalezdenisluis@gmail.com>
2022-07-18 11:56:56 +0200

    EmailValidator change Test

--- src/main/java/com/commerce/commerce/registration/RegistrationController.java
+++ src/main/java/com/commerce/commerce/registration/RegistrationController.java
@@ -4,10 +4,15 @@ package com.commerce.commerce.registration;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
 @RestController
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
+@Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+        flags = Pattern.Flag.CASE_INSENSITIVE)
 public class RegistrationController {
 
         private RegistrationService registrationService;
