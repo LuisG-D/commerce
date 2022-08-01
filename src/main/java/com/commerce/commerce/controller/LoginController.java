63db3076a524674447cfa9495319c51b2bc541b0
Luis Gonzalez <gonzalezdenisluis@gmail.com>
2022-08-01 02:02:52 +0200

    Add some Filter for MayoristaController

--- src/main/java/com/commerce/commerce/controller/LoginController.java
+++ src/main/java/com/commerce/commerce/controller/LoginController.java
@@ -8,11 +8,14 @@ import com.commerce.commerce.registration.RegistrationService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.Objects;
@@ -42,18 +45,25 @@ public class LoginController {
         return "Hola mundo";
     }
     @PostMapping("/signin")
-    public String authenticateUser(@Valid @RequestBody LoginRequest loginRequest, AppUser user) {
+    public ResponseEntity<UserDetails> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, AppUser user) {
 
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),
-                        loginRequest.getPassword()));
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),
+                            loginRequest.getPassword()));
 
-        if(Objects.nonNull(authentication)){
-            return userDetails.toString();
-        }else{
-            return "redirect:/login";
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+            if(Objects.nonNull(authentication)){
+                return new ResponseEntity<>(userDetails,null,HttpStatus.OK);
+            }else{
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Datos de usuario incorrectos");
+            }
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Datos de usuario incorrectos");
         }
+
     }
 
 
